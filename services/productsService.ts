4ff518b4d9db5cc907ab467e00249fb23f144345
picktherick <rickkque@gmail.com>
2025-05-31 18:37:48 -0300

    temp - set enums on productsService

--- services/productsService.ts
+++ services/productsService.ts
@@ -8,3 +8,20 @@ export const getProducts = async (): Promise<Product[]> => {
   if (response.data.length === 0) throw new Error("No products available");
   return response.data;
 };
+
+// Enums
+// {
+//   "PlateTypes": ["120", "145", "FAIXA"],
+//   "PlateMaterial": ["TEMPERADO", "LAMINADO", "TREFILADA"],
+//   "HoseType": ["PRENSADA", "MANGOTE"],
+//   "HoseAdapterPoint": ["RETA", "90", "90 LONGO"],
+//   "CableTieMaterial": ["ZINCADO_BRANCO", "ZINCADO_AMARELO", "INOX"],
+//   "ScrewFlange": ["MA", "MB", "MAQUINA", "SOBERBA", "AUTO_BROCANTE", "AUTO_TRAVANTE"],
+//   "ScrewNutsMaterial": ["ENEGRECIDO", "ZINCADO_BRANCO", "BICROMATIZADO", "INOX", "ACO"], //AÇO
+//   "NutsType": ["TORQUE", "TRAVANTE", "ALTA_DUPLA", "ALTA", "BAIXA"],
+//   "NutsFlange": ["MA", "MB", "MAQUINA"],
+//   "MaterialType": ["MATERIAL_LIMPEZA", "MATERIAL_ESCRITORIO", "INSUMO", "LIMPEZA"],
+//   "TubeType": ["RETANGULAR", "QUADRADO", "REDONDO"],
+//   "TubeMaterial": ["TEMPERADO", "LAMINADO", "TREFILADA"],
+//   "FormType": ["GENERIC", "PLATE", "HOSE", "ADAPTER", "CABLE_TIE", "SCREW", "NUTS", "MATERIAL", "TUBE"]
+// }
